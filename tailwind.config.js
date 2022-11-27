c628c3127aac12b944ab78f723dc3ddbf207f97b
Sagar <reyzon.thapa2020@gmail.com>
2022-11-27 19:24:21 +0545

    new

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   }
+// }
